5ec67d309cb669c55c8cb78716928bbeb7385ddb
adhith-c <adhithadhi17@gmail.com>
2023-01-10 11:30:19 +0530

    product add feature added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -16,17 +16,20 @@
 const express = require("express");
 const router = express.Router();
 // const { isAdmin, adminLogged } = require("../middleware");
-// const multer = require("multer");
+const multer = require("multer");
 // const methodOverride = require("method-override");
-// const { storage, cloudinary } = require("../cloudinary/index");
-// const upload = multer({
-//   storage,
-// });
+const { storage, cloudinary } = require("../cloudinary/index");
+const upload = multer({
+  storage,
+});
 
 // router.use(methodOverride("_method"));
 
 const { postAdminLogin } = require("../controllers/adminController");
-// const productController = require("../controller/productController");
+const {
+  viewProductAdmin,
+  postAddProducts,
+} = require("../controllers/productController");
 // const categoryController = require("../controller/categoryController");
 // const brandsController = require("../controller/brandController");
 // const orderController = require("../controller/orderController");
@@ -34,19 +37,14 @@ const { postAdminLogin } = require("../controllers/adminController");
 
 // router.get("/", adminController.getAdminLogin);
 router.post("/signin", postAdminLogin);
-//router.get("/dashboard", adminController.getDashboard);
+
 //<-------------------------------------------------------- PRODUCT Management-------------------------------------------------------------------->
 
-// // view Products
-// router.get("/viewproducts", productController.viewProductAdmin);
-// // Edit Product
+// view Products
+router.get("/viewproducts", viewProductAdmin);
+// Edit Product
 // router.get("/addproducts", productController.addProducts);
-// router.post(
-//   "/add-products",
-//   isAdmin,
-//   upload.array("image"),
-//   productController.postAddProducts
-// );
+router.post("/addproducts", upload.array("image"), postAddProducts);
 // // Edit Products
 // router.get("/editproducts/:id", isAdmin, productController.getEditProduct);
 // router.put(
