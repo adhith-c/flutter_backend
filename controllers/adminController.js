384e344e6524b18c8abdf27d41bff0f08e380363
adhith-c <adhithadhi17@gmail.com>
2023-01-26 10:31:25 +0530

    admin and product controller modified

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,111 +1,378 @@
-require("dotenv").config();
-const Admin = require("../models/admin");
-const User = require("../models/user");
-const Banner = require("../models/banner");
-const { hashPassword, comparePassword } = require("../utils/helper");
+const express = require("express");
+const router = express.Router();
 
-// exports.getAdminLogin = async (req, res) => {
-//   try {
-//     const admin = await Admin.find({});
-//     if (admin.length == 0) {
-//       const hashedpassword = hashPassword(process.env.PASSWORD);
-//       const newAdmin = new Admin({
-//         name: process.env.NAME,
-//         email: process.env.EMAIL,
-//         password: hashedpassword,
-//       });
-//       await newAdmin.save();
-//       res.send(newAdmin);
-//     } else {
-//       res.send("login page of admin");
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+const { urlencoded } = require("express");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const multer = require("multer");
 
-exports.postAdminLogin = async (req, res) => {
-  try {
-    const { mobileNumber, password } = req.body;
-    const admin = await Admin.findOne({ mobileNumber });
-    if (admin) {
-      console.log("admineee");
-      const isValid = comparePassword(password, admin.password);
-      if (isValid) {
-        const token = jwt.sign(
-          {
-            id: admin._id,
-            name: admin.name,
-            type: "admin",
+const { storage, cloudinary } = require("../cloudinary/index");
+const upload = multer({
+  storage,
+});
+const Product = require("../model/product");
+const User = require("../model/user");
+const Brand = require("../model/brand");
+const Coupon = require("../model/coupon");
+const Order = require("../model/order");
+const Banner = require("../model/banner");
+const asyncErrorCatcher = require("../util/asynErrorCatch");
+
+router.use(methodOverride("_method"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const getAdminLogin = (req, res) => {
+  res.render("admin/signin");
+};
+const postAdminLogin = (req, res) => {
+  // res.render('admin/index');
+  res.redirect("/admin/dashboard");
+};
+
+const viewUsers = async (req, res) => {
+  const users = await User.find({});
+  res.render("admin/viewUsers", {
+    users,
+  });
+};
+
+const logout = (req, res) => {
+  req.session.isAdmin = false;
+  res.redirect("/admin/");
+};
+
+const getDashboard = async (req, res, next) => {
+  let todaySale = 0,
+    totalSale = 0,
+    todayRevenue = 0,
+    totalRevenue = 0;
+  const todayOrders = await Order.find({
+    $and: [
+      {
+        createdAt: {
+          $lt: Date.now(),
+          $gt: Date.now() - 86400000,
+        },
+      },
+      {
+        paymentStatus: {
+          $ne: "notpaid",
+        },
+      },
+    ],
+  });
+  todayOrders.forEach((order) => {
+    todaySale += order.totalAmount;
+  });
+
+  const totalOrders = await Order.find({
+    paymentStatus: {
+      $eq: "paid",
+    },
+  });
+  totalOrders.forEach((order) => {
+    totalSale += order.totalAmount;
+  });
+  todayRevenue = (todaySale * 10) / 100;
+  totalRevenue = (totalSale * 10) / 100;
+  todayRevenue = parseFloat(todayRevenue).toFixed(2);
+  totalRevenue = parseFloat(totalRevenue).toFixed(2);
+  const graph = await Order.aggregate([
+    {
+      $group: {
+        _id: {
+          month: {
+            $month: "$createdAt",
           },
-          process.env.JWT_ADMIN_SECRET_KEY
-        );
-        res.status(200).json("login success");
-      } else {
-        res.status(409).json("incorrect password");
-      }
-    } else {
-      const hashedpassword = hashPassword(process.env.ADMIN_PASSWORD);
-      const newAdmin = new Admin({
-        name: process.env.ADMIN_NAME,
-        email: process.env.ADMIN_EMAIL,
-        mobileNumber: process.env.ADMIN_MOBILENUMBER,
-        password: hashedpassword,
-      });
-      await newAdmin.save();
-      const admin = await Admin.findOne({ mobileNumber });
-      if (admin) {
-        const isValid = comparePassword(password, admin.password);
-        if (isValid) {
-          const token = jwt.sign(
-            {
-              id: admin._id,
-              name: admin.name,
-              type: "admin",
-            },
-            process.env.JWT_ADMIN_SECRET_KEY
-          );
-          res.status(200).json("login success");
-        } else {
-          res.status(409).json("incorrect password");
-        }
-      } else {
-        res.status(403).json("error occured");
-      }
-    }
-  } catch (err) {
-    console.log(err);
-  }
+          day: {
+            $dayOfMonth: "$createdAt",
+          },
+          year: {
+            $year: "$createdAt",
+          },
+        },
+        totalPrice: {
+          $sum: "$totalAmount",
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $project: {
+        totalPrice: 1,
+        _id: 1,
+      },
+    },
+    {
+      $sort: {
+        _id: -1,
+      },
+    },
+    {
+      $limit: 7,
+    },
+  ]);
+  console.log(graph);
+  let values = [];
+  let revenue = [];
+  let graphlabels = [];
+  graph.forEach((g) => {
+    graphlabels.push(g._id);
+    values.push(g.totalPrice);
+    revenue.push((g.totalPrice * 10) / 100);
+  });
+  graphlabels = JSON.stringify(graphlabels);
+  console.log("graphlabels", graphlabels);
+  const recentSales = await Order.find({
+    paymentStatus: {
+      $eq: "paid",
+    },
+  });
+  const categorySale = await Order.aggregate([
+    {
+      $match: {
+        orderStatus: {
+          $nin: ["pending", "cancelled"],
+        },
+      },
+    },
+    {
+      $unwind: "$orderItems",
+    },
+    {
+      $project: {
+        total: {
+          $multiply: [
+            "$orderItems.productPrice",
+            "$orderItems.productQuantity",
+          ],
+        },
+        productId: "$orderItems.productId",
+        _id: 0,
+      },
+    },
+    {
+      $lookup: {
+        from: "products",
+        localField: "productId",
+        foreignField: "_id",
+        as: "products",
+      },
+    },
+    {
+      $unwind: "$products",
+    },
+    {
+      $project: {
+        total: "$total",
+        categoryName: "$products.category",
+      },
+    },
+    {
+      $group: {
+        _id: "$categoryName",
+        count: {
+          $sum: "$total", // $sum: "$orderItems.productQuantity"
+        },
+      },
+    },
+  ]);
+  let label = [];
+  let categoryValues = [];
+  console.log("categorysale");
+  console.log(categorySale);
+  categorySale.forEach((g) => {
+    categoryValues.push(g.count);
+    label.push(g._id);
+  });
+  const paymentStats = await Order.aggregate([
+    {
+      $group: {
+        _id: "$paymentType",
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        count: 1,
+      },
+    },
+  ]);
+  let paymentGraph = [];
+  paymentStats.forEach((payment) => {
+    paymentGraph.push(payment.count);
+  });
+  res.render("admin/index", {
+    todaySale,
+    totalSale,
+    todayRevenue,
+    totalRevenue,
+    values,
+    revenue,
+    recentSales,
+    paymentGraph,
+    categoryValues,
+    label,
+    graphlabels,
+  });
 };
-
-exports.userDetails = async (req, res) => {
+const blockUnblockUser = async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-exports.banners = async (req, res) => {
-  try {
-    const banners = await Banner.find({});
-    res.send(banners);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-exports.addBanners = async (req, res) => {
-  try {
-    const { offer, description } = req.body;
-    const newBanner = new Banner({
-      offer,
-      description,
+    let { id } = req.params;
+    const blockuser = await User.findOne({
+      _id: id,
     });
-    await newBanner.save();
-    res.send(newBanner);
-  } catch (err) {
-    console.log(err);
+    id = mongoose.Types.ObjectId(id);
+    let status = blockuser.status;
+    if (status == "active") {
+      await User.findByIdAndUpdate(id, {
+        isBlocked: true,
+        status: "blocked",
+      });
+      req.flash(
+        "success",
+        `Blocked the user ${blockuser.firstname} ${blockuser.lastname}`
+      );
+    } else {
+      await User.findByIdAndUpdate(id, {
+        isBlocked: false,
+        status: "active",
+      });
+      req.flash(
+        "success",
+        `unBlocked the user ${blockuser.firstname} ${blockuser.lastname}`
+      );
+    }
+    res.redirect("/admin/viewusers");
+  } catch (error) {
+    res.render("user/404");
   }
+};
+
+const getCoupon = async (req, res) => {
+  const coupons = await Coupon.find({});
+  res.render("admin/viewCoupon", {
+    coupons,
+  });
+};
+
+const getNewCoupon = (req, res) => {
+  res.render("admin/addCoupon");
+};
+
+const postNewCoupon = async (req, res) => {
+  const coupon = new Coupon(req.body);
+
+  await coupon.save();
+  console.log(coupon, "is the new brand");
+  req.flash("success", "coupon saved successfully");
+  res.redirect("/admin/viewcoupons");
+};
+
+const getEditCoupon = async (req, res) => {
+  const { id } = req.params;
+  const coupon = await Coupon.findById(id);
+  res.render("admin/editCoupon", {
+    coupon,
+  });
+};
+const putEditCoupon = async (req, res) => {
+  const { id } = req.params;
+  const coupon = await Coupon.findByIdAndUpdate(id, {
+    ...req.body,
+  });
+  console.log(coupon);
+  await coupon.save();
+  req.flash("success", "coupon edited successfully");
+  res.redirect("/admin/viewcoupons");
+};
+
+const deleteCoupon = async (req, res) => {
+  const { id } = req.params;
+  const deletecoupon = await Coupon.findByIdAndDelete(id);
+  console.log(deletecoupon);
+  res.redirect("/admin/viewcoupons");
+};
+
+const getBanner = async (req, res) => {
+  const banners = await Banner.find({});
+  res.render("admin/viewBanner", {
+    banners,
+  });
+};
+const getNewBanner = async (req, res) => {
+  const coupons = await Coupon.find({});
+  res.render("admin/addBanner", {
+    coupons,
+  });
+};
+
+const postNewBanner = async (req, res) => {
+  const obj = JSON.parse(JSON.stringify(req.body));
+  const banner = new Banner(obj);
+  banner.image = req.files.map((file) => ({
+    url: file.path,
+    filename: file.filename,
+  }));
+  await banner.save();
+  console.log(banner, "is the new banner");
+  req.flash("success", "banner saved successfully");
+  res.redirect("/admin/viewbanner");
+};
+const getEditBanner = async (req, res) => {
+  const { id } = req.params;
+  const banner = await Banner.findById(id);
+  res.render("admin/editBanner", {
+    banner,
+  });
+};
+const putEditBanner = async (req, res) => {
+  const { id } = req.params;
+  const banner = await Banner.findByIdAndUpdate(id, {
+    ...req.body,
+  });
+  console.log(banner);
+  await banner.save();
+  req.flash("success", "Banner edited successfully");
+  res.redirect("/admin/viewBanner");
+};
+const deleteBanner = async (req, res) => {
+  const { id } = req.params;
+  const deletebanner = await Banner.findByIdAndDelete(id);
+  console.log(deletebanner);
+  //await brands.save();
+  res.redirect("/admin/viewbanner");
+};
+
+module.exports = {
+  getAdminLogin,
+  postAdminLogin,
+  viewUsers,
+  logout,
+  getDashboard,
+  blockUnblockUser,
+  getCoupon,
+  getNewCoupon,
+  postNewCoupon,
+  getEditCoupon,
+  putEditCoupon,
+  deleteCoupon,
+  getBanner,
+  getNewBanner,
+  postNewBanner,
+  getEditBanner,
+  putEditBanner,
+  deleteBanner,
 };
--- controllers/productController.js
+++ controllers/productController.js
@@ -3,18 +3,26 @@ const express = require("express");
 const router = express.Router();
 
 const { urlencoded } = require("express");
-
-// const session = require("express-session");
-// const methodOverride = require("method-override");
+const passport = require("passport");
+const session = require("express-session");
+const methodOverride = require("method-override");
 const multer = require("multer");
 const mongoose = require("mongoose");
 const { storage, cloudinary } = require("../cloudinary/index");
 const upload = multer({
   storage,
 });
-const Product = require("../models/product");
+const Product = require("../model/product");
+const User = require("../model/user");
+const Brand = require("../model/brand");
+const Category = require("../model/category");
+const Order = require("../model/order");
+const Cart = require("../model/cart");
+const Wishlist = require("../model/wishlist");
 
-// router.use(methodOverride("_method"));
+const asyncErrorCatcher = require("../util/asynErrorCatch");
+
+router.use(methodOverride("_method"));
 
 cloudinary.config({
   cloud_name: "dinwlxluq",
@@ -22,132 +30,142 @@ cloudinary.config({
   api_secret: "AOqguffGbX94C6LPl4QoztTNws8",
 });
 
-exports.viewProductAdmin = async (req, res) => {
-  const products = await Product.find({});
-  res.json({ products });
-};
-exports.postAddProducts = async (req, res) => {
-  //res.redirect('/admin/viewproducts');  first check
+const viewProductUser = async (req, res) => {
   try {
-    console.log("body of postaddproducts", req.body);
-    const products = new Product(req.body);
-    products.image = req.files.map((file) => ({
-      url: file.path,
-      filename: file.filename,
-    }));
-
-    await products.save();
-    res.status(200).json("success");
-  } catch (err) {
-    console.log(err);
+    const { id } = req.params;
+    const products = await Product.findById(id);
+    const relatedProducts = await Product.find({
+      category: products.category,
+    });
+    let userId = req.session.userId;
+    const categories = await Category.find({});
+    userId = mongoose.Types.ObjectId(userId);
+    let cartCount = await Cart.aggregate([
+      {
+        $match: {
+          userId,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          count: {
+            $size: "$cartItems",
+          },
+        },
+      },
+    ]);
+    let wishlistCount = await Wishlist.aggregate([
+      {
+        $match: {
+          userId,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          count: {
+            $size: "$Items",
+          },
+        },
+      },
+    ]);
+    res.render("user/product", {
+      products,
+      relatedProducts,
+      cartCount,
+      wishlistCount,
+      categories,
+    });
+  } catch (error) {
+    res.render("user/404");
+    console.log(error);
   }
 };
 
-// const viewProductUser = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const products = await Product.findById(id);
-//     const relatedProducts = await Product.find({
-//       category: products.category,
-//     });
-//     let userId = req.session.userId;
-//     const categories = await Category.find({});
-//     userId = mongoose.Types.ObjectId(userId);
-//     let cartCount = await Cart.aggregate([
-//       {
-//         $match: {
-//           userId,
-//         },
-//       },
-//       {
-//         $project: {
-//           _id: 0,
-//           count: {
-//             $size: "$cartItems",
-//           },
-//         },
-//       },
-//     ]);
-//     let wishlistCount = await Wishlist.aggregate([
-//       {
-//         $match: {
-//           userId,
-//         },
-//       },
-//       {
-//         $project: {
-//           _id: 0,
-//           count: {
-//             $size: "$Items",
-//           },
-//         },
-//       },
-//     ]);
-//     res.render("user/product", {
-//       products,
-//       relatedProducts,
-//       cartCount,
-//       wishlistCount,
-//       categories,
-//     });
-//   } catch (error) {
-//     res.render("user/404");
-//     console.log(error);
-//   }
-// };
+const viewProductAdmin = async (req, res) => {
+  const products = await Product.find({});
+  res.render("admin/viewProducts", {
+    products,
+  });
+};
+const addProducts = async (req, res) => {
+  const category = await Category.find({});
+  const brand = await Brand.find({});
+  res.render("admin/addProducts", {
+    category,
+    brand,
+  });
+};
 
-// const addProducts = async (req, res) => {
-//   const category = await Category.find({});
-//   const brand = await Brand.find({});
-//   res.render("admin/addProducts", {
-//     category,
-//     brand,
-//   });
-// };
+const postAddProducts = async (req, res) => {
+  //res.redirect('/admin/viewproducts');  first check
+  console.log("body of postaddproducts", req.body);
+  const products = new Product(req.body);
+  products.image = req.files.map((file) => ({
+    url: file.path,
+    filename: file.filename,
+  }));
 
-// const getEditProduct = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const product = await Product.findById(id);
-//     res.render("admin/editProducts", {
-//       product,
-//     });
-//   } catch (error) {
-//     res.render("user/404");
-//   }
-// };
+  await products.save();
+  console.log(products, "is the new product");
+  req.flash("success", "product saved successfully");
+  res.redirect("/admin/viewproducts");
+};
 
-// const putEditProduct = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const obj = JSON.parse(JSON.stringify(req.body));
-//     const phots = req.files.map((f) => ({
-//       url: f.path,
-//       filename: f.filename,
-//     }));
-//     console.log(obj);
-//     const product = await Product.findByIdAndUpdate(id, obj);
-//     console.log(product);
-//     product.image.push(...phots);
-//     product.save();
-//     req.flash("success", "product edited successfully");
-//     res.redirect("/admin/viewproducts");
-//   } catch (error) {
-//     res.render("user/404");
-//     console.log(error);
-//   }
-// };
+const getEditProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("admin/editProducts", {
+      product,
+    });
+  } catch (error) {
+    res.render("user/404");
+  }
+};
 
-// const deleteProducts = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     // const deleteproducts = await Product.findByIdAndDelete(id);  permanent delete
-//     const deleteproducts = await Product.findByIdAndUpdate(id, {
-//       isDeleted: true,
-//     });
-//     console.log(deleteproducts);
-//     res.redirect("/admin/viewproducts");
-//   } catch (error) {
-//     res.render("user/404");
-//   }
-// };
+const putEditProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const obj = JSON.parse(JSON.stringify(req.body));
+    const phots = req.files.map((f) => ({
+      url: f.path,
+      filename: f.filename,
+    }));
+    console.log(obj);
+    const product = await Product.findByIdAndUpdate(id, obj);
+    console.log(product);
+    product.image.push(...phots);
+    product.save();
+    req.flash("success", "product edited successfully");
+    res.redirect("/admin/viewproducts");
+  } catch (error) {
+    res.render("user/404");
+    console.log(error);
+  }
+};
+
+const deleteProducts = async (req, res) => {
+  try {
+    const { id } = req.params;
+    // const deleteproducts = await Product.findByIdAndDelete(id);  permanent delete
+    const deleteproducts = await Product.findByIdAndUpdate(id, {
+      isDeleted: true,
+    });
+    console.log(deleteproducts);
+    res.redirect("/admin/viewproducts");
+  } catch (error) {
+    res.render("user/404");
+  }
+};
+
+module.exports = {
+  viewProductUser,
+  viewProductAdmin,
+  addProducts,
+  postAddProducts,
+  getEditProduct,
+  putEditProduct,
+  deleteProducts,
+};
